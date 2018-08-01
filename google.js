2084c119947c79ead0dbb6e7f8c8144cd65ded2f
DIVYANSH TRIPATHI <divyansh2998@icloud.com>
2018-08-01 13:48:13 +0530

    try catch added

--- google.js
+++ google.js
@@ -18,44 +18,50 @@ let getPlaceId = (method, latLng, next_page_token, name) => {
 
   C.req.get(request, (err, res, body) => {
     // console.log(JSON.parse(body).next_page_token.slice(0,3), "nextPageToken");
-    C.async.each(
-      JSON.parse(body).results,
-      (place, callback) => {
-        if (C._.lowerCase(place.vicinity).indexOf("vasant kunj") != -1) {
-          let id = require(`./id_vasant_kunj.json`);
+    try {
+      let locationList = JSON.parse(body).results;
+      C.async.each(
+        locationList,
+        (place, callback) => {
+          if (C._.lowerCase(place.vicinity).indexOf("vasant kunj") != -1) {
+            let id = require(`./id_vasant_kunj.json`);
 
-          id[0][`${place.name}`] = {
-            name: `${place.place_id}`,
-            lat: `${place.geometry.location.lat}`,
-            lng: `${place.geometry.location.lng}`,
-            visited: false
-          };
-          console.log("place inserted in object", place.name);
+            id[0][`${place.name}`] = {
+              name: `${place.place_id}`,
+              lat: `${place.geometry.location.lat}`,
+              lng: `${place.geometry.location.lng}`,
+              visited: false
+            };
+            console.log("place inserted in object", place.name);
 
-          C.fs.writeFile(`./id_vasant_kunj.json`, JSON.stringify(id), () => {
-            console.log("Wriiten on file");
+            C.fs.writeFile(`./id_vasant_kunj.json`, JSON.stringify(id), () => {
+              console.log("Wriiten on file");
+              callback();
+            });
+          } else {
+            console.log("Not in vicinity");
             callback();
-          });
-        } else {
-          console.log("Not in vicinity");
-          callback();
+          }
+        },
+        () => {
+          if (JSON.parse(body).next_page_token) {
+            console.log("it has next page");
+            getPlaceId(
+              null,
+              null,
+              JSON.parse(body).next_page_token,
+              "vasantkunj"
+            );
+          } else {
+            console.log("Everything over");
+            return;
+          }
         }
-      },
-      () => {
-        if (JSON.parse(body).next_page_token) {
-          console.log("it has next page");
-          getPlaceId(
-            null,
-            null,
-            JSON.parse(body).next_page_token,
-            "vasantkunj"
-          );
-        } else {
-          console.log("Everything over");
-          return;
-        }
-      }
-    );
+      );
+    } catch (e) {
+      // let locationList = {};
+      return;
+    }
   });
 };
 
